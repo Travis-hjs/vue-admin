fa7fc8a6ffdfc84b141ccb3711302ef6731234e0
1141752341@qq.com <1141752341@qq.com>
2020-11-14 23:23:48 +0800

    类型修改

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -17,18 +17,18 @@ export interface AjaxParams {
     /** 是否上传文件 通常是图片 */
     file?: FormData,
     /** 成功回调 */
-    success?: (
+    success?(
         /** 响应结果 */
         res: any,
         /** 响应原数据结果 */ 
         response: XMLHttpRequest
-    ) => void,
+    ): void,
     /** 失败回调 */
-    fail?: (value: XMLHttpRequest) => void,
+    fail?(value: XMLHttpRequest): void,
     /** 超时回调 */
-    timeout?: (value: XMLHttpRequest) => void,
+    timeout?(value: XMLHttpRequest): void,
     /** 请求进度 */
-    progress?: (event: ProgressEvent<XMLHttpRequestEventTarget>) => void
+    progress?(event: ProgressEvent<XMLHttpRequestEventTarget>): void
 }
 
 export interface RequestFail {
