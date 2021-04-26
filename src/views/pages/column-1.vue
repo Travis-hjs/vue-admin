ddc97e35c0ea3ee0935bad12450cd9b36ee2a9b4
黄景圣 <1141752341@qq.com>
2021-04-26 14:39:13 +0800

    增加demo

--- src/views/pages/column-1.vue
+++ src/views/pages/column-1.vue
@@ -1,18 +1,51 @@
 <template>
     <div class="column-1">
-        <el-alert :closable="false" :title="content" type="success" />
+        <el-alert :closable="false" :title="columnInfo.title" type="success" />
+        <el-divider content-position="left">响应数据</el-divider>
+        <el-card shadow="always">{{ columnInfo }}</el-card>
+        <div class="flex fvertical option_box">
+            <el-input class="f1" v-model="inputValue" style="margin-right: 16px;" placeholder="请输入内容" clearable></el-input>
+            <el-button @click="changeCount()" size="medium" type="primary" icon="el-icon-edit">设置共享数据的 input 值</el-button>
+        </div>
+        <el-divider content-position="left">hooks 代码</el-divider>
+        <el-input type="textarea" v-model="columnInfo.content" :autosize="{ minRows: 26, maxRows: 40}"></el-input>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import { columnInfo, updateColumnInfo } from "./hooks";
 
 export default defineComponent({
     setup() {
-        const content = "栏目-1";
+        const inputValue = ref("");
+
+        function changeCount() {
+            updateColumnInfo({
+                input: inputValue.value
+            })
+        }
+
         return {
-            content
+            columnInfo,
+            changeCount,
+            inputValue
         }
     }
 })
 </script>
+<style lang="scss">
+.column-1 {
+    .el-alert {
+        margin-bottom: 20px;
+    }
+    .option_box {
+        width: 100%;
+        max-width: 480px;
+        margin-bottom: 20px;
+    }
+    .el-card {
+        margin-bottom: 20px;
+    }
+}
+</style>
---
+++ src/views/pages/hooks.ts
@@ -0,0 +1,47 @@
+import { reactive } from "vue";
+import utils from "@/utils";
+
+interface ColumnInfoType {
+    title: string
+    content: string
+    input: string
+}
+
+/** 栏目信息 */
+export const columnInfo = reactive<ColumnInfoType>({
+    title: "利用 vue 3 新增的 reactive 实现共享数据，所以不需要 vuex 了；也就是 hooks",
+    content: `
+    import { reactive } from "vue";
+    import utils from "@/utils";
+    
+    interface ColumnInfoType {
+        title: string
+        content: string
+        input: number
+    }
+    
+    /** 栏目信息 */
+    export const columnInfo = reactive<ColumnInfoType>({
+        title: "",
+        content: "",
+        input: 0
+    });
+    
+    /**
+     * 更新栏目信息
+     * @param value 
+     */
+    export function updateColumnInfo(value: Partial<ColumnInfoType>) {
+        utils.modifyData(columnInfo, value);
+    }
+    `,
+    input: ""
+});
+
+/**
+ * 更新栏目信息
+ * @param value 
+ */
+export function updateColumnInfo(value: Partial<ColumnInfoType>) {
+    utils.modifyData(columnInfo, value);
+}
