c9f21d7ce739ba9ed3cd51d3e92333fbb958be3c
黄景圣 <1141752341@qq.com>
2021-05-28 12:32:32 +0800

    配置修改

--- vite.config.ts
+++ vite.config.ts
@@ -15,12 +15,6 @@ export default defineConfig({
     },
     server: {
         port: 3030,
-        /**
-         * 这边设置代理域名的时候，`request.ts`里面的`webUrl`请求域名要换成本机地址 => `location.origin`
-         * 打包到线上的时候则要把`request.ts`里面的`webUrl`请求域名换成正式环境域名
-         * 所以比较繁琐，干脆直接让后台打开跨域好了
-         */
-        // proxy: "http://10.0.18.207",
         // proxy: {
         //     "/api": {
         //         //要访问的跨域的域名
@@ -32,9 +26,7 @@ export default defineConfig({
         //         /** 开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样客户端端和服务端进行数据的交互就不会有跨域问题 */
         //         changOrigin: true,
         //         /** 对应上面的即可 */
-        //         pathRewrite: {
-        //             "^/api": "/api"
-        //         }
+        //         rewrite: (path) => path.replace(/^\/api/, '/api')
         //     }
         // }
     }
