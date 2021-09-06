79ad2d1195a1e66e8d2c5b3ab394cb2cf7ff3955
黄景圣 <1141752341@qq.com>
2021-09-06 14:31:52 +0800

    ts配置文件修改

--- src/types/index.ts
+++ src/types/index.ts
@@ -68,3 +68,13 @@ export interface ApiResult {
     /** 接口响应信息 */
     msg: string
 }
+
+/**
+ * 上传图片`change`回调类型
+ */
+export interface UploadChange<T = string | number> {
+    /** 和当前上传组件绑定的`id` */
+    id: T
+    /** 图片路径 */
+    src: string
+}
