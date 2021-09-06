30cd431b0ba8464c4f1fc90abf267f1088cffed0
黄景圣 <1141752341@qq.com>
2021-09-06 20:55:01 +0800

    配置模块调整

---
+++ src/utils/config.ts
@@ -0,0 +1,28 @@
+/**
+ * 项目配置模块
+ */
+const config = (function() {
+    const _env = process.env.NODE_ENV === "development" ? "dev" : "prod";
+
+    const url = {
+        dev: "http://wthrcdn.etouch.cn", // `http://${location.host}`,
+        prod: "http://wthrcdn.etouch.cn"
+    }
+
+    return {
+        /** 请求超时毫秒 */
+        get requestOvertime() {
+            return 8000;
+        },
+        /** `api`请求域名 */
+        get apiUrl() {
+            return url[_env];
+        },
+        /** 当前环境模式 */
+        get env() {
+            return _env;
+        }
+    }
+})();
+
+export default config;
