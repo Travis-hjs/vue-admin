7f4efeb1e959aeaf50ec880b8fe08ce7ef508b3c
黄景圣 <1141752341@qq.com>
2021-09-27 18:05:15 +0800

    菜单暂存更改

--- src/layout/components/Menu.vue
+++ src/layout/components/Menu.vue
@@ -2,10 +2,12 @@
     <MenuItem v-for="item in menuList" :key="item.path" :info="item" />
 </template>
 <script lang="ts">
-import { defineComponent, onMounted, provide, ref } from "vue";
+import { defineComponent, onMounted, ref, watchEffect } from "vue";
 import MenuItem from "./MenuItem.vue";
 import store from "@/store";
 import { filterHidden } from "@/router";
+import { useRoute } from "vue-router";
+import { LayoutMenuItem } from "@/types";
 
 export default defineComponent({
     name: "Menu",
@@ -13,14 +15,58 @@ export default defineComponent({
         MenuItem
     },
     setup(props, context) {
+        const route = useRoute();
+
         const layoutInfo = store.layout.info;
 
         const menuList = ref(filterHidden(store.layout.completeRouters));
-        
-        onMounted(function() {
-            console.log(menuList.value);
+    
+        let levels: Array<number> = [];
+
+        /**
+         * 更新菜单列表
+         * @param list
+         * @param level 层级数组
+         */
+        function updateMenuList(list: Array<LayoutMenuItem>, level: number) {
+            for (let i = 0; i < list.length; i++) {
+                const item = list[i];
+                item.isActive = item.path === route.path;
+                if (item.isActive) {
+                    levels[level] = i;
+                }
+                if (item.children && item.children.length > 0) {
+                    if (!item.isActive) {
+                        levels[level] = i;
+                    }
+                    updateMenuList(item.children, level + 1);
+                }
+            }
+        }
+
+        function update() {
+            levels = [];
+            updateMenuList(menuList.value, 0);
+            console.log("levels >>", levels);
+            let index = 0;
+            let item = menuList.value;
+            while (index < levels.length) {
+                item[levels[index]].isOpen = true;
+                if (item[levels[index]].children && item[levels[index]].children.length > 0) {
+                    item = item[levels[index]].children;
+                }
+                index++;
+            }
+        }
+
+        watchEffect(function() {
+            update()
         })
 
+        onMounted(function() {
+            update()
+        })
+        
         return {
             layoutInfo,
             menuList
