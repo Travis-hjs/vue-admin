6da3d6a1079d927bffe1246b547b67ba5d4dec32
黄景圣 <1141752341@qq.com>
2021-10-07 20:44:25 +0800

    demo

--- src/views/example/home.vue
+++ src/views/example/home.vue
@@ -1,22 +1,74 @@
 <template>
     <div class="home">
         <HelloWorld :msg="content" />
+        <p class="tips">横向滚动</p>
+        <div class="list-x">
+            <Scrollbar thumbColor="#87d068">
+                <div class="item" v-for="(item) in list" :key="item"></div>
+            </Scrollbar>
+        </div>
+        <p class="tips">垂直滚动</p>
+        <div class="list-y">
+            <Scrollbar :vertical="true" thumbColor="#87d068">
+                <div class="item" v-for="(item) in list" :key="item"></div>
+            </Scrollbar>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue";
+import Scrollbar from "@/components/Scrollbar/index.vue";
 
 export default defineComponent({
     components: {
-        HelloWorld
+        HelloWorld,
+        Scrollbar
     },
     setup() {
         const content = "首页";
+
         return {
-            content
+            content,
+            list: new Array(10).fill(0).map((_, index) => index)
         }
     }
 })
 </script>
+<style lang="scss">
+.home {
+    width: 100%;
+    .tips {
+        font-size: 16px;
+        color: #555;
+        padding: 8px 0;
+    }
+    .list-x {
+        width: 300px;
+        height: 88px;
+        .item {
+            display: inline-block;
+            width: 120px;
+            height: 100%;
+            background-color: tomato;
+        }
+        .item + .item {
+            margin-left: 14px;
+        }
+    }
+    .list-y {
+        width: 120px;
+        height: 400px;
+        .item {
+            width: 100%;
+            height: 88px;
+            margin-bottom: 14px;
+            background-color: tomato;
+            &:last-child {
+                margin-bottom: 0px;
+            }
+        }
+    }
+}
+</style>
--- src/views/page-404.vue
+++ src/views/page-404.vue
@@ -1,19 +1,24 @@
 <template>
     <div class="page-404">
-        <div></div>
+        页面不存在
     </div>
 </template>
 <script>
 import { defineComponent, reactive } from "vue";
 
 export default defineComponent({
+    name: "page-404",
     setup(props, context) {
         
+        return {
+            
+        }
     }
 })
 </script>
 <style lang="scss">
 .page-404 {
     width: 100%;
+    
 }
 </style>
