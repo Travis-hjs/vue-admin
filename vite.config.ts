327ad35cc4a3c100827dedb73c1bcf5ce109657c
黄景圣 <1141752341@qq.com>
2021-12-10 10:10:55 +0800

    注释调整

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     base: "./",
     resolve: {
         alias: {
-            "@": path.resolve(__dirname, "./src")
+            "@": path.resolve(__dirname, "src")
         }
     },
     server: {
