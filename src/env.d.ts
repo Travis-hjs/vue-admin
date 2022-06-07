d4ca0bb791c78f6fe47dcc1c7f3bb976031f44ea
黄景圣 <1141752341@qq.com>
2022-06-07 15:04:33 +0800

    类型调整

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,10 +7,3 @@ declare module "*.vue" {
     export default component
 }
 
-interface Window {
-    /**
-     * 当前版本，方便在控制台查看调试用
-     * @description 引用的是`package.json`中的`version`
-    */
-    version: string
-}
