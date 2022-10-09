ada0238a01cf632f8e1ddfeccba29cd47c943500
黄景圣 <1141752341@qq.com>
2022-10-09 16:33:38 +0800

    样式细节调整

--- src/components/Upload/Image.vue
+++ src/components/Upload/Image.vue
@@ -145,7 +145,7 @@ function removeImg() {
   .the-upload-content {
     background-color: transparent;
     border: 1px dashed #d9d9d9;
-    border-radius: 5px;
+    border-radius: $border-radius;
     overflow: hidden;
     @include time();
     &:hover { border-color: $theme; background-color: #fbfdff; }
