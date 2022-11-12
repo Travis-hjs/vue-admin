8ac7ed9a5125ab636dad444b45791d4fe1920e6c
黄景圣 <1141752341@qq.com>
2022-11-12 22:05:35 +0800

    新增js-cookie

---
+++ src/utils/cookie.ts
@@ -0,0 +1,36 @@
+import Cookies from "js-cookie";
+
+/**
+ * 获取`cookie`
+ * @param key 目标对象`key`值
+ */
+export function getCookie(key: string) {
+  return Cookies.get(key) || "";
+}
+
+/**
+* 设置`cookie`
+* @param key 目标对象`key`值
+* @param value 对应键值
+* @param options 配置项
+*/
+export function setCookie(key: string, value: string, options?: Cookies.CookieAttributes) {
+  if (options) {
+    Cookies.set(key, value, options);
+  } else {
+    Cookies.set(key, value);
+  }
+}
+
+/**
+* 移除指定`cookie`
+* @param key 目标对象`key`值
+* @param options 
+*/
+export function removeCookie(key: string, options?: Cookies.CookieAttributes) {
+  if (options) {
+    Cookies.remove(key, options);
+  } else {
+    Cookies.remove(key);
+  }
+}
