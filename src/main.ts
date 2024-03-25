7af1be6804a9d04da69b0c76773d05480e051e21
Travis <1141752341@qq.com>
2024-03-25 10:58:00 +0800

    新增全局组件声明

--- src/main.ts
+++ src/main.ts
@@ -41,3 +41,16 @@ app.component("base-dialog", BaseDialog);
 app.use(router);
 
 app.mount("#app");
+
+// ---------------- 声明全局组件标签类型提示 ----------------
+// declare module "@vue/runtime-core"
+declare module "vue" {
+  /**
+   * 自定义全局组件获得 Volar 提示
+   * - 自定义的全局组件需要在这里声明下才能获得 Volar 类型提示
+   */
+  export interface GlobalComponents {
+    BaseDialog: typeof BaseDialog
+    SvgIcon: typeof SvgIcon
+  }
+}
