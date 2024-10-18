85c730b666f1b6437e7167755ac373bc1ef474f3
Travis-黄景圣 <1141752341@qq.com>
2024-10-18 15:23:59 +0800

    feat: 新增curd入口组件

---
+++ src/components/Curd/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="curd-main-page">
+    <div></div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref, reactive } from "vue";
+
+</script>
+<style lang="scss">
+.curd-main-page {
+  width: 100%;
+}
+</style>
