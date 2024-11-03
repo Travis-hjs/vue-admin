f2abfefd53778021f8ebcf4783dfbd0530891a52
Travis-hjs <1141752341@qq.com>
2024-11-03 20:55:55 +0800

    配置页面暂存

--- src/views/example/curd-page.vue
+++ src/views/example/curd-page.vue
@@ -3,21 +3,158 @@
 </template>
 <script lang="ts" setup>
 import { getTableList } from "@/api/common";
-import { Curd, type CurdType } from "@/components/Curd";
+import { actionEditKey, actionProp, Curd, getColumnData, getFieldData, type CurdType } from "@/components/Curd";
+import { formatDate } from "@/utils";
 import { reactive } from "vue";
+
+const option = {
+  gameType: [
+    { label: "单机", value: 1 },
+    { label: "网游", value: 2 },
+  ]
+}
 
 const data = reactive<CurdType.Config>({
   search: {
     labelRight: false,
     labelWidth: 0,
-    list: [],
+    list: [
+      {
+        ...getFieldData("input", "gameName"),
+        label: "游戏名称"
+      },
+      {
+        ...getFieldData("input-between", "gamePrice"),
+        label: "游戏价格",
+        placeholder: ["最小价格", "最大价格"],
+        valueType: "array<number>"
+      },
+      {
+        ...getFieldData("select", "gameType"),
+        label: "游戏类型",
+        placeholder: "请选择游戏类型",
+        options: option.gameType
+      },
+      {
+        ...getFieldData("date", "gameDate"),
+        label: "上架日期",
+        dateType: "daterange",
+        formatShow: "YYYY-MM-DD HH:mm:ss",
+        format: "Y-M-D h:m:s",
+        valueType: "array",
+        valueWidth: 400
+      }
+    ],
   },
   table: {
-    columns: [],
-    actions: [],
-    selectKey: null,
-    formAdd: null,
-    formEdit: null,
+    columns: [
+      {
+        ...getColumnData("gameName", "游戏名称"),
+      },
+      {
+        ...getColumnData("gameType", "游戏类型"),
+        width: 140,
+        formatter(row, cellValue) {
+          const map = {
+            1: "单机",
+            2: "网游"
+          }
+          return map[cellValue as keyof typeof map] || "-";
+        }
+      },
+      {
+        ...getColumnData("gamePrice", "游戏价格"),
+        width: 140,
+        formatter(row, cellValue) {
+          return cellValue ? `￥${cellValue}` : "-";
+        }
+      },
+      {
+        ...getColumnData("date", "上架时间"),
+        width: 200,
+        formatter(row, cellValue) {
+          return cellValue ? formatDate(cellValue) : "-";
+        }
+      },
+      {
+        ...getColumnData(actionProp, "操作"),
+        width: 160,
+      },
+    ],
+    actions: [
+      {
+        key: actionEditKey,
+        text: "编辑",
+      },
+      {
+        key: "1",
+        text: "上报游戏",
+        click(row, index) {
+          console.log(row, index);
+        },
+      }
+    ],
+    selectKey: "id",
+    formAdd: {
+      width: 520,
+      labelPosition: "left",
+      labelWidth: 140,
+      fields: [
+        {
+          ...getFieldData("input", "gameName"),
+          label: "游戏名称",
+          required: true,
+          placeholder: "请输入游戏名称",
+        },
+        {
+          ...getFieldData("select", "gameType"),
+          label: "游戏类型",
+          required: true,
+          placeholder: "请选择游戏类型",
+          valueType: "number",
+          options: option.gameType
+        },
+        {
+          ...getFieldData("input", "gamePrice"),
+          label: "游戏价格",
+          required: true,
+          placeholder: "请输入游戏价格",
+          valueType: "number"
+        },
+        {
+          ...getFieldData("date", "gameDate"),
+          label: "上架日期"
+        }
+      ]
+    },
+    formEdit: {
+      width: 520,
+      labelPosition: "left",
+      labelWidth: 140,
+      fields: [
+        {
+          ...getFieldData("input", "gameName"),
+          label: "游戏名称",
+          required: true,
+          placeholder: "请输入游戏名称",
+        },
+        {
+          ...getFieldData("select", "gameType"),
+          label: "游戏类型",
+          required: true,
+          placeholder: "请选择游戏类型",
+          valueType: "number",
+          options: option.gameType
+        },
+        {
+          ...getFieldData("input", "gamePrice"),
+          label: "游戏价格",
+          required: true,
+          placeholder: "请输入游戏价格",
+          valueType: "number"
+        },
+      ]
+    },
   }
 });
 
