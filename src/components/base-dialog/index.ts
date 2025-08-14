6af7ece88f5894b8315e97163a73ef83f835e5dd
Travis-黄景圣 <1141752341@qq.com>
2025-08-14 11:46:58 +0800

    feat: 新增函数式调用弹框操作

--- src/components/base-dialog/index.ts
+++ src/components/base-dialog/index.ts
@@ -1,1 +1,85 @@
-export { default } from "./index.vue";
+import type { ExtractPropTypes } from "vue";
+import { createApp, h, ref } from "vue";
+import BaseDialog from "./index.vue";
+import { registerPlugins } from "@/utils/plugins";
+
+type DialogProps = ExtractPropTypes<InstanceType<typeof BaseDialog>>;
+
+interface DialogParams<Props> {
+  /** 弹框内容插槽组件 */
+  component: any;
+  /** 组件参数 */
+  props?: Props;
+  /** 弹框参数 */
+  dialogProps?: Partial<Omit<DialogProps, "show">>;
+  /** 确认回调 */
+  confirm?: () => void;
+  /** 确认按钮文字，默认为`"确认"` */
+  confirmText?: string;
+  /** 取消回调 */
+  cancel?: () => void;
+  /** 取消按钮文字，不传则没有取消操作 */
+  cancelText?: string;
+}
+
+/**
+ * 指令式打开弹框
+ * @param params 参数配置
+ */
+export function openDialog<Props extends object>(params: DialogParams<Props>) {
+  const {
+    component,
+    props = {},
+    dialogProps = {},
+    confirmText = "确认",
+    cancelText,
+    confirm,
+    cancel,
+  } = params;
+
+  const show = ref(false);
+
+  function onClose() {
+    show.value = false;
+    cancel && cancel();
+  }
+
+  function onClosed() {
+    app.unmount();
+    div.remove();
+  }
+
+  function onConfirm() {
+    show.value = false;
+    confirm && confirm();
+  }
+
+  const btnList = [h("button", { class: "the-btn blue", onClick: onConfirm }, confirmText)];
+
+  if (cancelText) {
+    btnList.unshift(h("button", { class: "the-btn", onClick: onClose }, cancelText));
+  }
+
+  const dialog = () => h(
+    BaseDialog,
+    { ...dialogProps, show: show.value, onClose, onClosed },
+    {
+      default: () => h(component, props),
+      footer: () => btnList,
+    }
+  );
+  
+  const app = createApp(dialog);
+
+  const div = document.createElement("div");
+  
+  document.body.appendChild(div);
+  
+  registerPlugins(app);
+
+  app.mount(div);
+  
+  show.value = true;
+}
+
+export { BaseDialog }
--- src/views/example/menu-1.vue
+++ src/views/example/menu-1.vue
@@ -1,7 +1,24 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { openDialog } from "@/components/base-dialog";
+import TsxExample from "../tsx/example";
+
+function onTest() {
+  openDialog({
+    component: TsxExample,
+    dialogProps: {
+      title: "函数调用式打开弹框",
+      width: "500px",
+    },
+    confirm() {
+      console.log("确 认");
+    }
+  });
+}
+</script>
 <template>
   <div class="menu-1">
     <span class="the-tag blue">menu-1</span>
+    <button class="the-btn blue ml-[10px]" v-ripple @click="onTest()">函数调用式打开弹框</button>
   </div>
 </template>
 <style lang="scss">
